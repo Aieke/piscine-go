aad8bfa9c8e9c1609744f0a2e1fa01e00ec70f83
aieke <aydos_sdu2007@mail.ru>
2019-10-16 15:10:35 +0600

    First

---
+++ isnegative/isnegative.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/01-edu/z01"
+
+
+func main() {
+	
+	isNegative(1)
+	isNegative(0)
+	isNegative(-1)
+
+z01.PrintRune('\n')
+}
+
+func isNegative(nb int) {
+	if nb >= 0 {
+		z01.PrintRune('F')
+	}else { 
+		z01.PrintRune('T')
+	}
+}
+
+
