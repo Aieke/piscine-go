1d3a15f6f9dd5c645b4d45e3881a2b90aadfac3b
aieke <aydos_sdu2007@mail.ru>
2019-10-16 16:21:40 +0600

    First

--- printcomb.go
+++ printcomb.go
@@ -3,12 +3,12 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := 48; i <= 57; i++ {
-		for j := i + 1; j <= 57; j++ {
-			for k := j + 1; k <= 57; k++ {
-				z01.PrintRune(rune(i))
-				z01.PrintRune(rune(j))
-				z01.PrintRune(rune(k))
+	for i := "48"; i <= "57"; i++ {
+		for j := "i + 1"; j <= "57"; j++ {
+			for k := "j + 1"; k <= "57"; k++ {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
 
 				if i != 55 || j != 56 || k != 57 {
 					z01.PrintRune(44)
