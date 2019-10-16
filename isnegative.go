79295d803a56de726c0bf80a7b1bd175c9a39a3e
aieke <aydos_sdu2007@mail.ru>
2019-10-16 15:21:49 +0600

    Fir

--- isnegative.go
+++ isnegative.go
@@ -2,15 +2,6 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func main() {
-
-	isNegative(1)
-	isNegative(0)
-	isNegative(-1)
-
-	z01.PrintRune('\n')
-}
-
 func IsNegative(nb int) {
 	if nb >= 0 {
 		z01.PrintRune('F')
