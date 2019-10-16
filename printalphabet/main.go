1205f12256a8d6bcb20cf688c485a0fdecc921de
aieke <aydos_sdu2007@mail.ru>
2019-10-16 13:01:30 +0600

    Fiteen

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -5,8 +5,8 @@ import "github.com/01-edu/z01"
 func main() {
     var lower = 'a'
 	for lower <= 'z' {
-	z01.PrintRune(rune(lower))
-	lower++
+		z01.PrintRune(rune(lower))
+		lower++
 	}
 	z01.PrintRune('\n')
 
