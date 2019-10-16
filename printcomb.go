219f713ee1a635807f0836f203dc55fc922550ae
aieke <aydos_sdu2007@mail.ru>
2019-10-16 16:07:42 +0600

    First

---
+++ printcomb.go
@@ -0,0 +1,20 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i:=48; i <= 57; i++ {
+		for j:=i+1; j<=57; j++ {
+			for k:=j+1; k<=57; k++ {
+				z01.PrintRune(rune(i))
+				z01.PrintRune(rune(j))
+				z01.PrintRune(rune(k))
+
+				if i!=55 || j!=56 || k!= 57 {
+					z01.PrintRune(44)
+					z01.PrintRune(32)
+				}
+			}
+		}
+	}
+}
