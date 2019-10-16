a5030f27f991f0189ba901acdcf8fef566e77b06
aieke <aydos_sdu2007@mail.ru>
2019-10-16 16:16:28 +0600

    First

--- printcomb.go
+++ printcomb.go
@@ -6,13 +6,13 @@ func PrintComb() {
 	for i := 48; i <= 57; i++ {
 		for j := i + 1; j <= 57; j++ {
 			for k := j + 1; k <= 57; k++ {
-				z01.PrintRune(rune(i))
-				z01.PrintRune(rune(j))
-				z01.PrintRune(rune(k))
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
 
 				if i != 55 || j != 56 || k != 57 {
-					z01.PrintRune(rune(44))
-					z01.PrintRune(rune(32))
+					z01.PrintRune(44)
+					z01.PrintRune(32)
 				}
 			}
 		}
