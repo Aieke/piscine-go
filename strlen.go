195d773b60f36b01b50d732f45d4d1bb2ce83927
aieke <aydos_sdu2007@mail.ru>
2019-10-17 12:48:41 +0600

    F

--- strlen.go
+++ strlen.go
@@ -1,12 +1,5 @@
 package piscine
 
 func StrLen(str string) int {
-	str := "Hello world!"
-	count := 0
-	for len(str) > 0 {
-		count++
-
-		str = str[:len(str)-size]
-	}
-	fmt.Println(count)
+	len([]rune(str))
 }
