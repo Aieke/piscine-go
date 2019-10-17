e126f4280bd53d42a53bba8fed6898b4a41de4d0
aieke <aydos_sdu2007@mail.ru>
2019-10-17 12:45:59 +0600

    F

--- strlen.go
+++ strlen.go
@@ -1,12 +1,9 @@
 package piscine
 
-import "unicode/utf8"
-
 func StrLen(str string) int {
 	str := "Hello world!"
 	count := 0
 	for len(str) > 0 {
-		_, size := utf8.DecodeLastRuneInString(str)
 		count++
 
 		str = str[:len(str)-size]
