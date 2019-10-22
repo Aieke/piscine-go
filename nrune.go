6c73a9ccf961d653c35add9701055259acf74b02
aieke <aydos_sdu2007@mail.ru>
2019-10-22 15:52:34 +0600

    A

--- nrune.go
+++ nrune.go
@@ -1,7 +1,25 @@
-package piscine
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	z01.PrintRune(NRune("Hello!", 3))
+	z01.PrintRune(NRune("Salut!", 2))
+	z01.PrintRune(NRune("Ola!", 4))
+	z01.PrintRune('\n')
+}
 
 func NRune(s string, n int) rune {
 	charRune := []rune(s)
-	nLtter := charRune[n]
-	return nLtter
+	len := 0
+	var nrune rune
+	for i := range charRune {
+		len = i + 1
+	}
+	if n >= 0 && n <= len {
+		nrune = []rune(s)[n-1]
+	}
+	return nrune
 }
