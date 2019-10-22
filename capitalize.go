fcbc356a47a241115714ef1eb740ffdb1d696a2f
aieke <aydos_sdu2007@mail.ru>
2019-10-22 19:47:28 +0600

    A

--- capitalize.go
+++ capitalize.go
@@ -13,7 +13,7 @@ func Capitalize(s string) string {
 
 		if strRuness[i] == 32 || strRuness[i] == '+' {
 			i++
-			if i != 0 && (!((runes[i-1] >= 'a' && runes[i-1] <= 'z') || (runes[i-1] >= 'A' && runes[i-1] <= 'Z') || (runes[i-1] >= '0' && runes[i-1] <= '9'))) {
+			if i != 0 && (!((strRuness[i-1] >= 'a' && strRuness[i-1] <= 'z') || (strRuness[i-1] >= 'A' && strRuness[i-1] <= 'Z') || (strRuness[i-1] >= '0' && strRuness[i-1] <= '9'))) {
 				if strRuness[i] >= 'a' && strRuness[i] <= 'z' {
 					strRuness[i] = rune(s[i] - 32)
 					continue
